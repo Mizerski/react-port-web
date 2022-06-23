26c75c73898944b57284a456ba3757e530682994
mizerski <mizerskimachado@hotmail.com>
2022-06-23 01:24:02 -0300

    Up 2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+    <div className="container header__container">
+      <h5>Hi I'm</h5>
+      <h1>Wesley Mizerski</h1>
+      <h5 className="text-light"> Fullstack Developer</h5>
+    </div>
+   </header>
   )
 }
 
